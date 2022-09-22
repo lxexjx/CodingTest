04d3a9c813d9f36b339b571fd8867616d02a324b
lxexjx <eun.leee1377@gmail.com>
2022-09-22 13:53:54 +0900

    commit P1546

---
+++ src/main/java/ch03/no1/P11659.java
@@ -0,0 +1,18 @@
+package ch03.no1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   //입력을 빨리 받기
+        StringTokenizer st = new StringTokenizer(bf.readLine());    //문자열 파싱
+
+        int n = Integer.parseInt(st.nextToken());   //항의 갯수
+        int m = Integer.parseInt(st.nextToken());   //질문이 몇개인지
+
+        long sum[]
+    }
+}
