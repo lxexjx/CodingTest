afe2b838be424e0b373e48230784e830f9027e9f
lxexjx <eun.leee1377@gmail.com>
2022-09-23 23:54:37 +0900

    commit P11659

--- src/main/java/ch03/no1/P11659.java
+++ src/main/java/ch03/no1/P11659.java
@@ -13,6 +13,18 @@ public class P11659 {
         int n = Integer.parseInt(st.nextToken());   //항의 갯수
         int m = Integer.parseInt(st.nextToken());   //질문이 몇개인지
 
-        long sum[]
+        long sum[] = new long[n + 1];
+
+        //누적 합 구하기
+        st = new StringTokenizer(bf.readLine());
+        for(int i=1;i<n;i++){
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(sum[b]-sum[a-1]);
+        }
     }
 }
